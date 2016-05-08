bf3033ae49503c5d3c78b33a1a00114e3f5733bb
Eugene Potapenko <eugene.john.potapenko@gmail.com>
2016-05-09 00:30:49 +0200

    infinity list

--- index.ios.js
+++ index.ios.js
@@ -13,42 +13,84 @@ import {Flexer, Spacer} from './utils.js';
 
 class ReactNativeGithubSearchDemo extends Component {
 
-  constructor(){
-    super();
-    this.getDataSource.bind(this);
-  }
+  constructor(props) {
+    super(props);
 
-  getInitialState(){
-    return {pos: 0, searchResult:[]}
+    this.onSearch = this.onSearch.bind(this);
+    this.renderRow = this.renderRow.bind(this);
+    this.updateDataSource = this.updateDataSource.bind(this);
+    this.onRefresh = this.onRefresh.bind(this);
+    this.onInfinite = this.onInfinite.bind(this);
+    this.loadedAllData = this.loadedAllData.bind(this);
+
+    this.state = {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+      loading: false,
+    }
   }
 
   onSearch(e) {
-    console.log("on-search", e);
+    console.log("on refresh");
+    setTimeout(() => {
+      this.list.hideHeader();
+      this.updateDataSource([]);
+    }, 400);
   }
 
-  componentWillMount() {
-    
-  }
-
-  getDataSource(){
-    console.log("this.state >>> ", this, this.state)
+  updateDataSource(result) {
     var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-    return ds.cloneWithRows(this.state.searchResult);
+    this.setState({dataSource: ds.cloneWithRows(result)});
+  }
+
+  renderRow(data) {
+    return <View><Text>{data}</Text></View>
+  }
+
+  renderEmptyRow() {
+    return (
+      <View style={styles.row}>
+        <Flexer/>
+        <View>
+          <Spacer height={20}/>
+          <Text style={{fontSize:12, color: "rgba(0,0,0,0.1)"}}>
+            have no data
+          </Text>
+        </View>
+        <Flexer/>
+      </View>
+    )
+  }
+
+  onInfinite() {
+    console.log("on infinite");
+  }
+
+  loadedAllData() {
+    return true;
   }
 
   render() {
     return (
 
-      <View style2={styles.container}>
+      <View style={[styles.container]}>
         <Spacer height={30}/>
         <SearchBar
           ref='searchBar'
           placeholder='Search'
           onChangeText={this.onSearch}
         />
-        <RefreshInfiniteListView 
-          ref = {(list) => {this.list= list}}
-          dataSource={this.getDataSource()}/>
+        <RefreshInfiniteListView
+          ref={(list) => {this.list= list}}
+          style={[styles.flex]}
+          dataSource={this.state.dataSource}
+          renderRow={this.renderRow}
+          renderEmptyRow={this.renderEmptyRow}
+          onRefresh={this.onSearch}
+          onInfinite={this.onInfinite}
+          loadedAllData={this.loadedAllData}
+        />
       </View>
 
     );
@@ -59,6 +101,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#F5FCFF',
+  },
+  red: {
+    backgroundColor: "red"
+  },
+  row: {
+    flexDirection: "row"
+  },
+  flex: {
+    flex: 1
   },
   resultList: {
     flex: 1,
